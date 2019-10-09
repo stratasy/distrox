35ead1ca8683ab0daa5295dc33bdfaf258f08bbb
Eric Zhang <ezhang887@gmail.com>
2019-10-08 22:13:27 -0500

    Add comments

--- client.go
+++ client.go
@@ -7,11 +7,15 @@ import (
 )
 
 func main() {
+    // connect to localhost:8080
     conn, err := net.Dial("tcp", "localhost:8080")
     if err != nil {
         return;
     }
+    // send a message
     fmt.Fprintf(conn, "The client is sending a message!\n")
+
+    // and print the response
     status, err := bufio.NewReader(conn).ReadString('\n')
     if err != nil {
         return
--- server.go
+++ server.go
@@ -15,15 +15,20 @@ func handleConnection(conn net.Conn) {
 }
 
 func main() {
+    // startup server on localhost:8080
     ln, err := net.Listen("tcp", ":8080")
     if err != nil {
         return;
     }
+    // infinite loop
     for {
+        // wait for clients to connect
 	conn, err := ln.Accept()
 	if err != nil {
             return;
 	}
+
+        // and when one connects, asynchronously go send a response
 	go handleConnection(conn)
     }
 }
