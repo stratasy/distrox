7dff3aa0a8bba3414f50c81fe9da6674155467e9
Eric Zhang <ezhang887@gmail.com>
2019-10-23 04:07:13 -0500

    Update proxy to work with firefox

--- proxy_server.go
+++ proxy_server.go
@@ -2,22 +2,19 @@ package main
 
 import (
     "net/http"
-    "io/ioutil"
+    "io"
     "fmt"
-    "strings"
 )
 
 func Redir(w http.ResponseWriter, r *http.Request) {
-    // strip: /proxy/www.google.com -> www.google.com
-    request_path := strings.TrimPrefix(r.URL.Path, "/proxy/")
-    // append http://
-    request_path = fmt.Sprintf("http://%s", request_path)
+    // format new request
+    request_path := fmt.Sprintf("%s://%s%s", r.URL.Scheme, r.URL.Host, r.URL.Path)
 
     // create new HTTP request with the target URL (everything else is the same)
     new_request, err := http.NewRequest(r.Method, request_path, r.Body)
 
     // send request to server
-    fmt.Printf("Sending HTTP %s request to %s\n", r.Method, request_path)
+    fmt.Printf("Sending %s request to %s\n", r.Method, request_path)
 
     client := &http.Client{}
     res, err := client.Do(new_request)
@@ -25,17 +22,25 @@ func Redir(w http.ResponseWriter, r *http.Request) {
         fmt.Println(err)
         return
     }
+    defer res.Body.Close()
+
+    // copy the headers over to thre ResponseWriter. 
+    //res.Header is a map of string -> slice (string)
+    for key, slice := range res.Header {
+        for _, val := range slice {
+            w.Header().Add(key, val)
+        }
+    }
 
     // forward response to client
-    body, err := ioutil.ReadAll(res.Body)
+    _, err = io.Copy(w, res.Body)
     if err != nil {
         fmt.Println(err)
         return
     }
-    fmt.Fprintf(w, string(body))
 }
 
 func main() {
-    http.HandleFunc("/proxy/", Redir)
+    http.HandleFunc("/", Redir)
     http.ListenAndServe(":8080", nil)
 }
